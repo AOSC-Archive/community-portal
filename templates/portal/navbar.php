f9f2b9f88bd2b7b6fa72dc1cd508855675c9d7d2
LionNatsu <372691006@qq.com>
2014-07-26 13:55:18 +0800

    Smart Arrow

--- templates/portal/navbar.php
+++ templates/portal/navbar.php
@@ -1,9 +1,9 @@
     <div class="blog-masthead WQYMicroHei">
       <div class="container">
         <nav class="blog-nav">
-          <a class="blog-nav-item active" href="http://projects-dev.anthonos.org/">Portal</a>
-          <a class="blog-nav-item" href="http://projects-dev.anthonos.org/news/">News</a>
-          <a class="blog-nav-item" href="http://projects-dev.anthonos.org/about/">About</a>
+          <a class="blog-nav-item <?php if($_SERVER["SCRIPT_NAME"]=="/index.php") echo "active"; ?>" href="/">Portal</a>
+          <a class="blog-nav-item <?php if($_SERVER["SCRIPT_NAME"]=="/news/index.php") echo "active"; ?>" href="/news">News</a>
+          <a class="blog-nav-item <?php if($_SERVER["SCRIPT_NAME"]=="/about/index.php") echo "active"; ?>" href="/about">About</a>
         </nav>
       </div>
     </div>
