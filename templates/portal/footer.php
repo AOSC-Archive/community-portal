8a56851c68e86990de728e85fb52b5f37285881d
lmy441900 <826531041@qq.com>
2014-08-12 16:15:42 +0800

    Added WebFont and fixed the missing </p>

--- templates/portal/footer.php
+++ templates/portal/footer.php
@@ -4,16 +4,18 @@
 			<div class="col-md-12 col-sm-12 footer-block">
 				<div class="col-md-4 col-sm-4">
 					<h4 align="left">Main Sites</h4>
-					<p align="left"><a href="/">Community Portal</a>
-					<p align="left"><a href="/aosc-os/">AOSC OS</a>
-			    <p align="left"><a href="/ast/">Anthon Starter</a>
-		      <p align="left"><a href="/linkc/">LinkC</a>
-					<p align="left"><a href="https://github.com/AOSC-Dev/" target=_blank>GitHub Repository</a>
+					<p align="left"><a href="/">Community Portal</a></p>
+					<p align="left"><a href="/aosc-os/">AOSC OS</a></p>
+			    <p align="left"><a href="/ast/">Anthon Starter</a></p>
+		      <p align="left"><a href="/linkc/">LinkC</a></p>
+					<p align="left"><a href="https://github.com/AOSC-Dev/" target=_blank>GitHub Repository</a></p>
 				</div>
 				<div class="col-md-4 col-sm-4">
 					<h4 align="left">Community Sites</h4>
-					<p align="left"><a href="https://bugs.anthonos.org/" target=_blank>Bug Site</a></p>
-					<p align="left"><a href="https://wiki-dev.anthonos.org/" target=_blank>TechBase</a></p> <!--"-dev"-->
+					<p align="left"><a href="https://bugs.anthonos.org/" target=_blank>Bug Tracking</a></p>
+                    <!-- "wiki-dev" is not avaliable yet.
+					<p align="left"><a href="https://wiki-dev.anthonos.org/" target=_blank>TechBase</a></p> -->
+                    <p align="left"><a href="https://wiki.anthonos.org/" target=_blank>Wiki Page</a></p>
 					<p align="left"><a href="http://l10n.anthonos.org/" target=_blank>Localization</a></p>
 				</div>
 				<div class="col-md-4 col-sm-4 ">
@@ -38,7 +40,11 @@
 			<div class="col-md-12 col-sm-12">
 				<div class="col-sm-9">
 					<br />
-					<p align="left">Powered by <a href="http://getbootstrap.com/" target=_blank>Bootstrap 3.x</a>. Page designed by Taylor Yang, Duminhui and Jeff Bai.</p>
+					<p align="left">
+                    Powered by <a href="http://getbootstrap.com/" target=_blank>Bootstrap 3.x</a>. Page designed by Taylor Yang, Duminhui and Jeff Bai.
+                    <br />
+                    Fonts by <a href="http://glyphicons.com/" target=_blank>Glyphicons</a> and <a href="http://sourceforge.net/projects/sourcesans.adobe/" target=_blank>Adobe Source Sans Pro</a>.
+                    </p>
 				</div>
 			</div>
 		</div>
