6798c50aa4a6d21ce93a86309d418ad8fe749da7
Arthur Wang <arthur200126@gmail.com>
2014-09-15 15:34:31 +0800

    Some SEO Stuffs...

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no">
-    <meta name="description" content="Homepage of AOSC OS">
+    <meta name="description" content="Homepage of AOSC OS 安同社区首页">
+    <meta name="keywords" content="AOSC,OS,Anthon,AnthonOS,安同,Linux,GNU/Linux,GNU,开源,社区,Open Source,Community">
     <link rel="icon" href="/img/favicons/aosc.ico">
     <title>Community Portal - Home</title>
 
