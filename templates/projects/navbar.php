500d00fcd96ed2532773e30f1af75256623632d9
Jeff Bai <jeffbaichina@members.fsf.org>
2014-11-01 19:30:08 -0600

    Simplify-sorry for much more commits, web editing

--- templates/projects/navbar.php
+++ templates/projects/navbar.php
@@ -3,11 +3,9 @@
     <nav class="blog-nav">
       <a class="blog-nav-item <?php if($_SERVER["SCRIPT_NAME"]=="/index.php") echo "active"; ?>" href="/">Portal</a>
       <a class="blog-nav-item <?php if($_SERVER["SCRIPT_NAME"]=="/projects/index.php") echo "active"; ?>" href="/projects">Projects</a>
+      <a class="blog-nav-item <?php if($_SERVER["SCRIPT_NAME"]=="/about/index.php") echo "active"; ?>" href="/about">About</a>
       <a class="blog-nav-item" href="http://l10n.anthonos.org">Localization</a>
       <a class="blog-nav-item" href="http://wiki-dev.anthonos.org">Documentation</a>
-      <a class="blog-nav-item" href="https://github.com/AOSC-Dev">Git</a>
-      <a class="blog-nav-item" href="https://pastebin.anthonos.org">Pastebin</a>
-      <a class="blog-nav-item <?php if($_SERVER["SCRIPT_NAME"]=="/about/index.php") echo "active"; ?>" href="/about">About</a>
     </nav>
   </div>
 </div>
