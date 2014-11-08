3efcf71dfb0d6cb11a7c4b1adfa795e860ba8486
Arthur Wang <arthur200126@gmail.com>
2014-11-08 23:45:32 +0800

    Forced to stop by parents. One month.

---
+++ aosc-os/oobp.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<?php
+define('IN_FRAME', true);
+
+$langs = array(
+	'en' => array (
+		'h1' => 'Out Of Box Preview',
+		'h-desc' => 'Alpha-level release of AOSC OS3',
+		'h2' => 'Be Brave...',
+		'p-desc' => 'It looks fresh, and clean this time',
+		'p-1' => 'It\'s almost 2 months since the official start of AOSC OS3 project, while we are still struggling
+to maintain AOSC OS2 with loads of its issues coming, we are ready to show off some of our maintained AOSC OS3 releases.',
+		'p-2' => 'These Out Of Box Preview, for now, comes in the form of tarball (for now); a variety of editions
+(spins if you prefer) are released.',
+		'p-3' => 'These are some OOBP releases available:',
+		'links' => 'And here are the download links:',
+		'download' => 'Download',
+		'with' => 'with',
+		'instht' => ' ',
+		'insthtsteps' => ' ',
+		'greet' => ' ',
+	),
+	'zh-CN' => array (
+		'h1' => '开箱即用预览版',
+		'h-desc' => 'AOSC OS3 Alpha 级别测试',
+		'h2' => '勇敢点啦！',
+		'p-desc' => '这次的系统，干净清爽',
+		'p-1' => '离 AOSC OS3 的开始已经有几个月了。尽管我们还在对付 AOSC OS2 一堆又一堆的问题报告和建议，但我们已经准备好
+炫耀我们全新的系统版本了。',
+		'p-2' => '这些“开箱即用”版本目前以 tar 文件的形式给出，我们提供各种各样的版本。',
+		'p-3' => '看一眼吧：',
+		'links' => '……然后来下载吧！',
+		'download' => '下载',
+		'with' => '带',
+		'instht' => ' ',
+		'insthtsteps' => ' ',
+		'greet' => ' ',
+	),
+	'zh-TW' => array (
+		'title' => ' ',
+		'h1' => '',
+		'h-desc' => '',
+		'h2' => ' ',
+		'p-desc' => ' ',
+		'p-1' => ' ',
+		'p-2' => ' ',
+		'p-3' => ' ',
+		'links' => ' ',
+		'download' => ' ',
+		'with' => ' ',
+		'instht' => ' ',
+		'insthtsteps' => ' ',
+		'greet' => ' ',
+	),
+);
+
+include 'templates/lang.php' ?>
+
+<html lang=<?=$lang?>>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no">
+		<meta name="description" content="Summary of AOSC OS Out of the box preview">
+		<meta name="keywords" content="AOSC OS OOBP Preview Out Of Box 安同 Linux 开箱即用 预览 OS3">
+		<meta name="author" content="Junde Yi<lmy441900@gmail.com>, Jeff Bai <jeffbaichina@members.fsf.org>">
+		<link rel="icon" href="/img/favicons/aosc-os.ico">
+
+		 <title>AOSC OS - OOBP</title>
+
+		<!-- Bootstrap core CSS -->
+		<link href="/css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- Custom styles for this template -->
+		<link href="/css/blog.css" rel="stylesheet">
+
+	</head>
+
+	<body>
+		<?php include '../../templates/aosc-os/navbar.php';?>
+		<div class="container">
+			<div class="blog-header">
+			<img src="/img/os3.svg" height="150" width="150" align=right hspace="40" >
+				<h1 class="blog-title"><?=$langs[$lang]['h1']?></h1>
+				<p class="lead blog-description"><?=$langs[$lang]['h-desc']?></p>
+			</div>
+			<div class="row">
+				<div class="col-sm-9 blog-main">
+					<div class="blog-post">
+					<h2 class="blog-post-title"><?=$langs[$lang]['h2']?></h2>
+					<p class="lead blog-description"><?=$langs[$lang]['p-desc']?></p>
+					<p><?=$langs[$lang]['p-1']?></p>
+					<p><?=$langs[$lang]['p-2']?></p>
+					<p><?=$langs[$lang]['p-3']?></p>
+					<br /><br />
+					<?php
+						for ($a=1;$a<=3;$a++) echo '<img src="/img/oobp-logo/'.$a.'.png" id="img'.$a.'" alt="img'.$a.'" width="48">';?>
+					<br /><br />
+					<p><h3><?=$langs[$lang]['link']?></h3></p>
+						<ul class="nav nav-tabs" id="myTab">
+							<li> OOBP with Enlightenment, <a href="http://repo.anthonos.org/os3-releases/os3-next/OOBP/aosc-os3_oobp-enlightenment_mountain-of-faith_amd64_dpkg_stripped_20140821.tar.xz">DPKG/Apt</a>, <a href="http://repo.anthonos.org/os3-releases/os3-next/OOBP/aosc-os3_oobp-enlightenment_mountain-of-faith_amd64_rpm_stripped_20140821.tar.xz">RPM/Yum</a></li>
+							<li> OOBP with Pantheon, <a href="http://repo.anthonos.org/os3-releases/os3-next/OOBP/aosc-os3_oobp-pantheon_fairy-wars_amd64_dpkg_stripped_20140821.tar.xz">DPKG/Apt</a>, <a href="http://repo.anthonos.org/os3-releases/os3-next/OOBP/aosc-os3_oobp-pantheon_fairy-wars_amd64_rpm_stripped_20140821.tar.xz">RPM/Yum</a></li>
+							<li> OOBP with Xfce, <a href="http://repo.anthonos.org/os3-releases/os3-next/OOBP/aosc-os3_oobp-xfce_scarlet-devil_amd64_dpkg_stripped_20140821.tar.xz">DPKG/Apt</a>, <a href="http://repo.anthonos.org/os3-releases/os3-next/OOBP/aosc-os3_oobp-xfce_scarlet-devil_amd64_rpm_stripped_20140821.tar.xz">RPM/Yum</a></li>
+						</ul>
+            <div class="tab-content">
+              
+              <div class="tab-pane active" id="home">Your Home tab content here </div>
+            </div>
+					<br />
+          <p><h3>How do I use them?</h3></p>
+          <p>If you would really want to use a distribution in its Alpha stage, the mandatory skill would be
+          RTFM (no, we mean Read The Fine Manuals). Basic steps to install and use these tarball releases were
+          shown as below:</p>
+          <ul>
+            <li> Get a LiveCD or boot up another Linux distribution (must be x86_64);</li>
+            <li> Download the tarball(s);</li>
+            <li> Clear a partition (of course you can seperate /home, /boot, etc);</li>
+            <li> Mount the partition (TFM for "mount" command, <a href="http://linux.die.net/man/8/mount">here</a>);</li>
+            <li> Untar the tarball(s) (TFM for "tar" command, <a href="http://linux.die.net/man/1/tar">here</a>);</li>
+            <li> Configure domain name resolver (cp /etc/resolv.conf /path/to/mount/point/etc/);</li>
+            <li> Mount and bind /dev /proc /sys to OOBP;</li>
+            <li> "chroot" into OOBP (TFM for "chroot" command, <a href="http://linux.die.net/man/2/chroot">here</a>);</li>
+            <li> Install GRUB (search it up, man...);</li>
+            <li> Unmount the partition, and reboot;</li>
+            <li> Boom (if this happenned, report it on IRC or throw it on the <a href="http://bugs.anthonos.org">bug tracker</a>);</li>
+          </ul><br />
+          <p>Happy hacking...</p>
+          </div>
+				</div> <!-- 
+      </div><!-- col -->
+		</div><!-- /.row -->
+		<?php include '../../templates/portal/footer.php';?>
+
+		<!-- Bootstrap core JavaScript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="/js/jquery-min.js"></script>
+		<script src="/js/bootstrap.min.js"></script>
+	</body>
+</html>
---
+++ aosc-os/summary.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang=zh-CN>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no">
+    <meta name="description" content="Summary of AOSC OS">
+    <meta name="author" content="Junde Yi<lmy441900@gmail.com>, Jeff Bai <jeffbaichina@members.fsf.org>">
+    <link rel="icon" href="/img/favicons/aosc-os.ico">
+
+    <title>AOSC OS - Summary</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/blog.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <?php include '../../templates/aosc-os/navbar.php';?>
+
+    <div class="container">
+
+      <div class="blog-header">
+      <img src="/img/os3.svg" height="150" width="150" align=right hspace="40" >
+        <h1 class="blog-title">AOSC OS</h1>
+        <p class="lead blog-description">What we are doing now...</p>
+      </div>
+
+      <div class="row">
+
+        <div class="col-sm-9 blog-main">
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">AOSC OS3</h2>
+            <p class="lead blog-description">the Yellow circular badge</p>
+            <p>AOSC OS3 is the distribution building project for the year 2014 - 2015, based on the established base of
+            AOSC OS2. Updates, changes are lined up for the year. Project "Nirvana" are intended as the new distribution
+            branch with RPM package management. Here below are the final projects intended for the year:
+            <ul>
+              <li> AnthonOS, for desktop-oriented Linux distribution</li>
+              <li> CentralPoint, for server-oriented Linux distribution</li>
+              <li> Nirvana, for RPM branch of desktop-oriented Linux distribution</li>
+            </ul>
+          </div>
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">AOSC OS2</h2>
+            <p class="lead blog-description">the Blue circular badge</p>
+            <p>AOSC OS2 is the distribution building project for the year 2013 - 2014, this is the first generation of our
+            community's independent distribution. Two final products are intended for this generation of project:
+            <ul>
+              <li> AnthonOS, for desktop-oriented Linux distribution</li>
+              <li> CentralPoint, for server-oriented Linux distribution</li>
+            </ul>
+          </div>
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">AOSC OS1</h2>
+            <p class="lead blog-description">the Red circular badge</p>
+            <p>AOSC OS1 is the distribution <b>customization</b> project for the year 2011 - 2013, this is the first generation of our
+            community's distribution project. Only one final products are intended for this generation of project:
+            <ul>
+              <li> AnthonOS, for desktop-oriented Linux distribution</li>
+            </ul>
+            <p>This version of AnthonOS is only a Debian derivative with customized KDE desktop, and it was never maintained after its
+            final release as a measure to kickstart AOSC OS2 to make CentralPoint and AnthonOS into a unified distribution.
+          </div>
+
+        </div>
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+    <?php include '../../templates/portal/footer.php';?>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="/js/jquery-min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+  </body>
+</html>
