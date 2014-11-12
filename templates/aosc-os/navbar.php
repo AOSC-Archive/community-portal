b45d0befbffdac1114b3afa8551a00944e4e2bd5
Jeff Bai <jeffbaichina@members.fsf.org>
2014-11-12 15:11:52 -0700

    Update navbar.php

--- templates/aosc-os/navbar.php
+++ templates/aosc-os/navbar.php
@@ -2,8 +2,8 @@
       <div class="container">
         <nav class="blog-nav">
           <a class="blog-nav-item <?php if($_SERVER["SCRIPT_NAME"]=="/aosc-os/index.php") echo "active"; ?>" href="/aosc-os/">News</a>
-		  <a class="blog-nav-item <?php if($_SERVER["SCRIPT_NAME"]=="/aosc-os/oobp/index.php") echo "active"; ?>" href="/aosc-os/oobp/">OOBP</a>
-          <a class="blog-nav-item <?php if($_SERVER["SCRIPT_NAME"]=="/aosc-os/summary/index.php") echo "active"; ?>" href="/aosc-os/summary/">Summary</a>
+          <a class="blog-nav-item <?php if($_SERVER["SCRIPT_NAME"]=="/aosc-os/oobp/index.php") echo "active"; ?>" href="/aosc-os/oobp/">OOBP</a>
+          <a class="blog-nav-item <?php if($_SERVER["SCRIPT_NAME"]=="/aosc-os/summary/index.php") echo "active"; ?>" href="/aosc-os/summary/">History</a>
           <a class="blog-nav-item <?php if($_SERVER["SCRIPT_NAME"]=="/aosc-os/screenshot/index.php") echo "active"; ?>" href="/aosc-os/screenshot/">Screenshot</a>
           <a class="blog-nav-item <?php if($_SERVER["SCRIPT_NAME"]=="/aosc-os/about/index.php") echo "active"; ?>" href="/aosc-os/about/">About</a>
         </nav>
