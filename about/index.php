e7f212f26c4ea07d84d29a86a7d9230f2a272af1
Jeff Bai <jeffbaichina@members.fsf.org>
2014-11-12 19:07:52 -0700

    portal area fully translated to zh_CN

--- about/index.php
+++ about/index.php
@@ -2,111 +2,32 @@
 define('IN_FRAME', true);
 $langs = array(
   'en' => array(
-    'title'=>'Community Portal - AOSC',
+    'title'=>'Community Portal - About',
     'portal'=>'Portal',
     'projects'=>'Projects',
     'about'=>'About',
     'localization'=>'Localization',
     'docs'=>'TechBase',
-  ),
-  'zh-cn' => array(
-    'title'=>'关于我们 - 安同开源社区',
-    'portal'=>'门户',
-    'projects'=>'项目',
-    'about'=>'关于',
-    'localization'=>'本地化',
-    'docs'=>'知识库',
-  ),
-  'zh-tw' => array(
-    'title'=>'關於我們 - 安同開源社區',
-    'portal'=>'門戶',
-    'projects'=>'項目',
-    'about'=>'關於',
-    'localization'=>'本地化',
-    'docs'=>'知識庫',
-  ),
-);
-include '../templates/lang.php';
-?>
-
-<html lang="<?php echo $lang;?>">
-  <head>
-    <meta name="generator"
-    content="HTML Tidy for HTML5 (experimental) for Windows https://github.com/w3c/tidy-html5/tree/c63cc39" />
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no" />
-    <meta name="description" content="Summary of AOSC OS" />
-    <meta name="author" content="Junde Yi&lt;lmy441900@gmail.com&gt;, Jeff Bai &lt;jeffbaichina@members.fsf.org&gt;" />
-    <link rel="icon" href="/img/favicons/aosc.ico" />
-    <title>Community Portal - About</title>
-    <!-- Bootstrap core CSS -->
-    <link href="/css/bootstrap.min.css" rel="stylesheet" />
-    <!-- Custom styles for this template -->
-    <link href="/css/blog.css" rel="stylesheet" />
-  </head>
-  
-  <body><?php include '../templates/portal/navbar.php';?>
-  <div class="row jumbotron">
-    <div class="container section-welcome">
-      <div class="row">
-        <div class="col-sm-10 col-sm-offset-1">
-          <div class="col-sm-8 col-xs-12">
-            <h1>About Us</h1>
-            <p>We are not mystery, but we implement miracles.</p>
-          </div>
-          <div class="col-sm-3 col-sm-offset-1 shortcuts hidden-xs">
-            <img src="/img/aosc.png" />
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-9 col-sm-offset-1">
-        <div class="row">
-          <h1>Who are we?</h1>
-          <br />
-          <p>We are members of this open source community, some of us are students and the others may be people working for their
-          own lives and families.</p>
-        </div>
-        <hr class="divider" />
-        <div class="row">
-          <h1>What do we do?</h1>
-          <br />
-          <p>As members of this open source (oriented) community, of cource we are fighting for improvement of open source software
-          and solutions. To do this:</p>
-          <ul>
-            <li>We build better Linux distribution for ourselves and Linux lovers;</li>
-            <li>We improve localization of open source software and actually contributes them;</li>
-            <li>We open participation for more opportunities to build open source software together;</li>
-            <li>We don&#39;t waste time on propaganda and speeches, when not necessary;</li>
-          </ul>
-        </div>
-        <hr class="divider" />
-        <div class="row">
-          <h1>How do I participate?</h1>
-          <br />
-          <p>For now, the best way to participate is to join the community group.</p>
+    'about-title'=>'About Us',
+    'about-text'=>'We are not mystery, we just talk less.',
+    'who-are-we'=>'Who are we?',
+    'who-text'=>'We are members of this open source community, some of us are students and the others may be people working for their own lives and families.',
+    'what-do'=>'What do we do?',
+    'what-text'=>'As members of this open source (oriented) community, we fight for improvement of open source software and solutions. To achieve our goal:',
+    'what-list'=>'<li>We build better Linux distribution for ourselves and Linux lovers;</li>
+            <li>We improve open source software by actually contributes them;</li>
+            <li>We open participation for more people to build open source software better, together;</li>
+            <li>We don&#39;t waste time on propaganda and speeches, when not necessary;</li>',
+    'how-participate'=>'How do I participate?',
+    'how-text'=>'For now, the best way to participate is to join the community group.
           <ul>
             <li>for IRC users go to: #anthon at freenode.net;</li>
             <li>for QQ users (mainly from Mainland China) go to group: 125016052</li>
-          </ul>
-          <div class="alert alert-info" role="alert">
-            <p>
-              <b>These 2 groups are binded with each other, so what you&#39;ve said in one side will be reposted to another. No log
-              will be made.</b>
-            </p>
-          </div>
-        </div>
-        <hr class="divider" />
-        <div class="row">
-          <h1>Thanks!</h1>
-          <br />
-          <p>There are good guys all around the world that provided us with good server and network resources!</p>
-          <p>Here&#39;s some of the awesome ones we want to mention of:</p>
+          </ul>',
+    'group-info'=>'These 2 groups are binded with each other, so what you&#39;ve said on one end will be passed to another. No log will be made.',
+    'thanks'=>'Thanks!',
+    'thank-list'=>'There are good guys all around the world that provided us with good server and network resources!</p>
+          <p>Here&#39;s some of the most awesome ones we want to mention of:</p>
           <ul>
             <li>
             <a href="http://www.ucloud.com/">UCloud</a> - where this website is hosted on.</li>
@@ -122,7 +43,167 @@ include '../templates/lang.php';
             <a href="http://www.maxcdn.com/">MaxCDN</a> - awesome CDN resources for repository mirrors.</li>
             <li>
             <a href="http://crowdin.net/">Crowdin</a> - localization hosting.</li>
+          </ul>',
+  ),
+  'zh-cn' => array(
+    'title'=>'社区门户 - 关于',
+    'portal'=>'门户',
+    'projects'=>'项目',
+    'about'=>'关于',
+    'localization'=>'本地化',
+    'docs'=>'知识库',
+    'about-title'=>'关于我们',
+    'about-text'=>'我们不是大神，我们只是少说废话。',
+    'who-are-we'=>'我们是谁？',
+    'who-text'=>'我们是这个开源社区的成员，部分是学生，而另一部分，是为生活和家庭打拼的人们。',
+    'what-do'=>'我们做些什么？',
+    'what-text'=>'作为这个开源（主导的）社区的成员，我们为开源软件和解决方案的改进不懈奋斗着。为实现我们的目标：',
+    'what-list'=>'<li>我们为自己以及 Linux 爱好者构建更好的 Linux 发行版；</li>
+    <li>我们以实际的代码贡献改进开源软件；</li>
+    <li>我们开放参与，让更多的人参与进来，构建更好的开源软件；</li>
+    <li>我们不屑于在不必要的时候过多炫耀自己；</li>',
+    'how-participate'=>'如何参与进来？',
+    'how-text'=>'目前来说，最好的参与方式是加入我们的聊天组。
+    <ul>
+      <li>IRC 用户请加入 freenode.net 频道 #anthon;</li>
+      <li>QQ 用户请加入群 125016052;</li>
+    </ul>',
+    'group-info'=>'这两个聊天组是相互绑定的，在其中一边的发言将会被传递到另一边。我们不保存任何聊天日志。',
+    'thanks'=>'多谢！',
+    'thank-list'=>'世界各地的学校和企业给我们提供了许多的网络和服务器资源，好人一生平安！</p>
+          <p>如下是我们的特别感谢：</p>
+          <ul>
+            <li>
+            <a href="http://www.ucloud.com/">UCloud，优刻得</a> - 该站点的托管商</li>
+            <li>
+            <a href="http://www.ncnu.edu.tw/">国立暨南大学（台湾）</a> - 我们的公用编译服务器</li>
+            <li>
+            <a href="http://studio.bluet.org/">BlueT</a> - 我们的软件仓库托管商</li>
+            <li>
+            <a href="http://www.upyun.com/">Upyun，又拍云</a> - CDN 资源托管</li>
+            <li>
+            <a href="http://www.qiniutek.com/">Qiniu，七牛云</a> - CDN 资源托管</li>
+            <li>
+            <a href="http://www.maxcdn.com/">MaxCDN</a> - 软件仓库 CDN 镜像</li>
+            <li>
+            <a href="http://crowdin.net/">Crowdin</a> - 本地化系统托管</li>
+          </ul>',
+  ),
+  'zh-tw' => array(
+    'title'=>'Community Portal - About',
+    'portal'=>'門戶',
+    'projects'=>'項目',
+    'about'=>'關於',
+    'localization'=>'本地化',
+    'docs'=>'知識庫',
+    'about-title'=>'About Us',
+    'about-text'=>'We are not mystery, we just talk less.',
+    'who-are-we'=>'Who are we?',
+    'who-text'=>'We are members of this open source community, some of us are students and the others may be people working for their own lives and families.',
+    'what-do'=>'What do we do?',
+    'what-text'=>'As members of this open source (oriented) community, we fight for improvement of open source software and solutions. To achieve our goal:',
+    'what-list'=>'
+            <li>We build better Linux distribution for ourselves and Linux lovers;</li>
+            <li>We improve open source software by actually contributes them;</li>
+            <li>We open participation for more people to build open source software better, together;</li>
+            <li>We don&#39;t waste time on propaganda and speeches, when not necessary;</li>',
+    'how-participate'=>'How do I participate?',
+    'how-text'=>'For now, the best way to participate is to join the community group.</p>
+          <ul>
+            <li>for IRC users go to: #anthon at freenode.net;</li>
+            <li>for QQ users (mainly from Mainland China) go to group: 125016052</li>
+          </ul>',
+    'group-info'=>'These 2 groups are binded with each other, so what you&#39;ve said on one end will be passed to another. No log will be made.',
+    'thanks'=>'Thanks!',
+    'thank-list'=>'There are good guys all around the world that provided us with good server and network resources!</p>
+          <p>Here&#39;s some of the most awesome ones we want to mention of:</p>
+          <ul>
+            <li>
+            <a href="http://www.ucloud.com/">UCloud</a> - where this website is hosted on.</li>
+            <li>
+            <a href="http://www.ncnu.edu.tw/">National Chi Nan University</a> - our public compiling server.</li>
+            <li>
+            <a href="http://studio.bluet.org/">BlueT</a> - awesome server for repository.</li>
+            <li>
+            <a href="http://www.upyun.com/">Upyun</a> - CDN contents.</li>
+            <li>
+            <a href="http://www.qiniutek.com/">Qiniu</a> - CDN contents.</li>
+            <li>
+            <a href="http://www.maxcdn.com/">MaxCDN</a> - awesome CDN resources for repository mirrors.</li>
+            <li>
+            <a href="http://crowdin.net/">Crowdin</a> - localization hosting.</li>
+          </ul>',
+  ),
+);
+include '../templates/lang.php';
+?>
+
+<html lang="<?php echo $lang;?>">
+  <head>
+    <meta name="generator"
+    content="HTML Tidy for HTML5 (experimental) for Windows https://github.com/w3c/tidy-html5/tree/c63cc39" />
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no" />
+    <meta name="description" content="Summary of AOSC OS" />
+    <meta name="author" content="Junde Yi&lt;lmy441900@gmail.com&gt;, Jeff Bai &lt;jeffbaichina@members.fsf.org&gt;" />
+    <link rel="icon" href="/img/favicons/aosc.ico" />
+    <title><?php echo $langs[$lang]['title'];?></title>
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet" />
+    <!-- Custom styles for this template -->
+    <link href="/css/blog.css" rel="stylesheet" />
+  </head>
+  
+  <body><?php include '../templates/portal/navbar.php';?>
+  <div class="row jumbotron">
+    <div class="container section-welcome">
+      <div class="row">
+        <div class="col-sm-10 col-sm-offset-1">
+          <div class="col-sm-8 col-xs-12">
+            <h1><?php echo $langs[$lang]['about-title'];?></h1>
+            <p><?php echo $langs[$lang]['about-text'];?></p>
+          </div>
+          <div class="col-sm-3 col-sm-offset-1 shortcuts hidden-xs">
+            <img src="/img/aosc.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-9 col-sm-offset-1">
+        <div class="row">
+          <h1><?php echo $langs[$lang]['who-are-we'];?></h1>
+          <br />
+          <p><?php echo $langs[$lang]['who-text'];?></p>
+        </div>
+        <hr class="divider" />
+        <div class="row">
+          <h1><?php echo $langs[$lang]['what-do'];?></h1>
+          <br />
+          <p><?php echo $langs[$lang]['what-text'];?></p>
+          <ul><?php echo $langs[$lang]['what-list'];?>
           </ul>
+        </div>
+        <hr class="divider" />
+        <div class="row">
+          <h1><?php echo $langs[$lang]['how-participate'];?></h1>
+          <br />
+          <p><?php echo $langs[$lang]['how-text'];?></p>
+          <div class="alert alert-info" role="alert">
+            <p>
+              <b><?php echo $langs[$lang]['group-info'];?></b>
+            </p>
+          </div>
+        </div>
+        <hr class="divider" />
+        <div class="row">
+          <h1><?php echo $langs[$lang]['thanks'];?></h1>
+          <br />
+          <p><?php echo $langs[$lang]['thank-list'];?>
         </div>
       </div>
     </div>
