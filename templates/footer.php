b0ea53dc2631fc0e9c85174b4f1f2bfb7cb69baf
lmy441900 <826531041@qq.com>
2014-12-27 19:04:13 +0800

    Add OpenHub widget and a little traslation

--- templates/footer.php
+++ templates/footer.php
@@ -3,12 +3,18 @@ if (!defined('IN_FRAME')) exit();
 $langs_f = array(
   'en' => array(
     'main'=>'Main Sites',
+    'community'=>'Community Sites',
+    'contact'=>'Contact Us',
   ),
   'zh-cn' => array(
     'main'=>'主要站点',
+    'community'=>'社区相关',
+    'contact'=>'联系我们',
   ),
   'zh-tw' => array(
     'main'=>'主要站點',
+    'community'=>'Community Sites',
+    'contact'=>'Contact Us',
   ),
 );
 include_once $_SERVER['DOCUMENT_ROOT'].'/templates/lang.php';
@@ -25,13 +31,13 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/templates/lang.php';
           <p align="left"><a href="https://github.com/AOSC-Dev/" rel="external nofollow">GitHub Repository</a></p>
         </div>
         <div class="col-sm-4">
-          <h4 align="left">Community Sites</h4>
+          <h4 align="left"><?php echo $langs_f[$lang]['community'];?></h4>
           <p align="left"><a href="https://bugs.anthonos.org/" rel="external nofollow">Bug Tracker</a></p>
           <p align="left"><a href="http://wiki-dev.anthonos.org/" rel="external nofollow">TechBase</a></p>
           <p align="left"><a href="http://l10n.anthonos.org/" rel="external nofollow">Localization</a></p>
         </div>
         <div class="col-sm-4 ">
-          <h4 align="left">Contact Us</h4>
+          <h4 align="left"><?php echo $langs_f[$lang]['contact'];?></h4>
           <p align="left"><a href="http://weibo.com/anthondev/" rel="external nofollow">Sina Weibo</a></p>
           <p align="left"><a href="irc://irc.freenode.org/#anthon" rel="external nofollow">IRC Channel</a></p>
           <p align="left"><a href="http://shang.qq.com/wpa/qunwpa?idkey=ab7dd682e389d0432bf71e7446ea2ecb923499c49b146e4c25717ecbb15ffb03" rel="external nofollow">QQ Group</a></p>
@@ -48,13 +54,16 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/templates/lang.php';
         </div>
         <div class="col-sm-9" style="margin-top:20px;">
           <p align="left">
-          Powered by <a href="http://getbootstrap.com/" rel="external nofollow">Bootstrap 3.x</a>. Page designed by Lion Yang, Duminhui and Jeff Bai.
+            Powered by <a href="http://getbootstrap.com/" rel="external nofollow">Bootstrap 3.x</a>. Page designed by Lion Yang, Duminhui and Jeff Bai.
           </p>
           <p align="left">
-          <a href="http://www.miibeian.gov.cn/">粤ICP备14040623号</a>
+            Fonts by <a href="http://glyphicons.com/" rel="external nofollow">Glyphicons</a> and <a href="http://sourceforge.net/projects/sourcesans.adobe/" rel="external nofollow">Adobe Source Sans Pro</a>.
           </p>
           <p align="left">
-          Fonts by <a href="http://glyphicons.com/" rel="external nofollow">Glyphicons</a> and <a href="http://sourceforge.net/projects/sourcesans.adobe/" rel="external nofollow">Adobe Source Sans Pro</a>.
+            <a href="http://www.miibeian.gov.cn/">粤ICP备14040623号</a>
+          </p>
+          <p align="left">
+            <script type="text/javascript" src="http://www.openhub.net/p/717872/widgets/project_thin_badge.js"></script>
           </p>
         </div>
       </div>
