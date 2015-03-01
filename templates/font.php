d5f85c2533b396ad253f9b2a636eb1d4b0791374
LionNatsu <372691006@qq.com>
2015-03-01 14:34:37 +0800

    Font: Microsoft YaHei UI Light #2

--- templates/font.php
+++ templates/font.php
@@ -1,7 +1,26 @@
 <?php
 if (!defined('IN_FRAME')) exit();
 
-echo '<style type="text/css">body,h1,.h1,h2,.h2,h3,.h3,h4,.h4,h5,.h5,h6,.h6{font-family:';
+echo '<style type="text/css">';
+echo 'body,h1,.h1,h2,.h2,h3,.h3,h4,.h4,h5,.h5,h6,.h6{font-family:';
+if (isset ($lang)){
+  switch ($lang){
+    case 'zh-cn':
+    case 'zh-tw':
+    case 'zh-hk':
+    case 'zh':
+      echo "source-sans, 'Segoe UI', 'Lantinghei SC', 'Lantinghei TC', 'Xin Gothic', 'Microsoft YaHei UI', 'Heiti SC', sans-serif;";
+      break;
+    
+    case 'en-us':
+    case 'en':
+    default:
+      echo 'source-sans, sans-serif;';
+      break;
+  }
+}else{}
+echo '}';
+echo 'h1,.h1,h2,.h2,h3,.h3,h4,.h4,h5,.h5,h6,.h6{font-family:';
 if (isset ($lang)){
   switch ($lang){
     case 'zh-cn':
@@ -17,8 +36,7 @@ if (isset ($lang)){
       echo 'source-sans, sans-serif;';
       break;
   }
-}else{
-  
-}
-echo '}</style>';
+}else{}
+echo '}';
+echo '</style>';
 ?>
