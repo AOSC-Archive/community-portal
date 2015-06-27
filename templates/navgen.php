490e45f22cb6df8c1f03bbfc62b49f7bf30f227a
Mingye Wang <arthur200126@gmail.com>
2015-06-27 19:00:53 +0800

    navgen: PRIDE!

--- templates/navgen.php
+++ templates/navgen.php
@@ -1,6 +1,9 @@
 <?php
 if (!defined('IN_FRAME')) exit();
+$_pride_color=array('#dc1c29', '#fb8f34', '#fedc45', '#47c53d', '#3270d3', '#7625a8');
 function pushNavBar(){
+  global $_pride_navcnt;
+  $_pride_navcnt=0;
   echo <<<EOD
   <div class="blog-masthead">
     <div class="container">
@@ -9,10 +12,19 @@ EOD;
   return;
 }
 function addNavItem($phpPath, $href, $lang_selector, $langs_array, $level='normal'){
+  global $_pride_color, $_pride_navcnt;
   echo '<a class="blog-nav-item';
   if($_SERVER["SCRIPT_NAME"]==$phpPath) echo ' active';
-  if($level!='normal') echo ' '.$level;
-  echo '" href="'.$href.'">';
+  if($level != 'normal') echo ' '.$level;
+  echo '" href="'.$href.'"';
+  /* _pride_do - OH NO PHP */
+  if($level == 'normal') {
+	$_pride_id='_nav_norm_'.$_pride_navcnt;
+	echo ' id="'.$_pride_id.'" onmouseover="_pride_s(\''.$_pride_color[$_pride_navcnt].'\',\''.$_pride_id;
+	echo '\')" onmouseout="_pride_u(\''.$_pride_id.'\')"';
+	$_pride_navcnt++;
+  }
+  echo '>';
   echo $langs_array[$lang_selector];
   echo '</a>';
   return;
@@ -26,4 +38,6 @@ function popNavBar(){
 EOD;
   return;
 }
-?>
+?><script>function _pride_s(color,id){ document.getElementById(id).style.backgroundColor=color; }
+function _pride_u(id){ document.getElementById(id).style.backgroundColor=''; }
+</script>
